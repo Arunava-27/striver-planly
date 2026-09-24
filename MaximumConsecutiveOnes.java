4b8222915d90ceef7ae26079abcdcdedb7e79bf9
Arunava-27 <drarunkundu22@gmail.com>
2026-09-24 16:26:31 +0530

    add MaximumConsecutiveOnes class with findMaxConsecutiveOnes method and main method for testing

---
+++ MaximumConsecutiveOnes.java
@@ -0,0 +1,25 @@
+public class MaximumConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                if(maxCount < count){
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 1, 1, 0};
+        int res = new MaximumConsecutiveOnes().findMaxConsecutiveOnes(arr);
+        System.out.println("Maximum Consecutive Ones : "+res);
+    }
+}
--- SecondLargestElement.java
+++ SecondLargestElement.java
@@ -1,22 +1,24 @@
 public class SecondLargestElement {
     public int secondLargestElement(int[] nums) {
-        int largest = nums[0];
-        int secondLargest = Integer.MIN_VALUE;
+        int largest = nums[0]; // Initialize largest to the first element of the array
+        
+        int secondLargest = Integer.MIN_VALUE; // Initialize secondLargest to the smallest possible value
 
+        // Iterate through the array to find the largest and second largest elements
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > largest) {
+            if (nums[i] > largest) { // Found a new largest element, update secondLargest as well
                 secondLargest = largest;
                 largest = nums[i];
-            } else if (nums[i] > secondLargest && nums[i] < largest) {
+            } else if (nums[i] > secondLargest && nums[i] < largest) { // Found a new second largest element
                 secondLargest = nums[i];
             }
         }
 
-        if (secondLargest == Integer.MIN_VALUE) {
+        if (secondLargest == Integer.MIN_VALUE) { // No second largest element found, return -1
             return -1;
         }
 
-        return secondLargest;
+        return secondLargest; // Return the second largest element found
     }
 
     public static void main(String[] args) {
