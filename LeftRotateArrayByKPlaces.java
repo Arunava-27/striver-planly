ae41358ef303121c98d67e0fda5a531050b1e53b
Arunava-27 <drarunkundu22@gmail.com>
2026-09-24 19:04:10 +0530

    add LeftRotateArrayByKPlaces class with rotateArray method and main method for testing

---
+++ LeftRotateArrayByKPlaces.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class LeftRotateArrayByKPlaces {
+    public void rotateArray(int[] nums, int k) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7};
+        LeftRotateArrayByOne lr = new LeftRotateArrayByOne();
+        lr.rotateArrayByOne(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
